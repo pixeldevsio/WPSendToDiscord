454013231e4ce9f66adb67c03b32b822cbc2c121
Pixel Devs <admin@pixeldevs.io>
2021-12-08 13:05:17 -0500

    Update PHPCS standards

--- discord.php
+++ discord.php
@@ -1,23 +1,32 @@
+<?php
+
+/**
+ * Send_message_to_discord
+ *
+ * @param  string $message The message to send.
+ *
+ * @return void
+ */
 function send_message_to_discord( $message = 'undefined' ) {
-	
-	// get your webhook URL from Discord
+
+	// Get your webhook URL from Discord.
 	$url = 'https://discordapp.com/api/webhooks/your-webhook-id-here';
-	$currentDate = date("l F dS Y h:i:sa", current_time( 'timestamp', 0 ));
-	
+	$current_date = gmdate( 'l F dS Y h:i:sa' );
+
 	$data = array(
-		'content' => $message . ' - ' . $currentDate
+		'content' => $message . ' - ' . $current_date
 	);
 
 	// use key 'http' even if you send the request to https://...
 	$options = array(
-	    'http' => array(
-	        'header'  => "Content-Type: application/json",
-	        'method'  => 'POST',
-	        'content' => json_encode($data)
-	    )
+		'http' => array(
+			'header'  => 'Content-Type: application/json',
+			'method'  => 'POST',
+			'content' => json_encode( $data ),
+		),
 	);
-	$context  = stream_context_create($options);
-	$result = file_get_contents($url, false, $context);
+	$context = stream_context_create( $options );
+	$result  = file_get_contents( $url, false, $context );
 
 }
 add_action( 'wp_ajax_discord_message', 'send_message_to_discord' );
